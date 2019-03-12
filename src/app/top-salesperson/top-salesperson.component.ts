ec864d52df902d3d347bbd0f6e54df0a3a48035d
Maxime DUROYON <maxime.duroyonJR@gmail.com>
2019-03-12 14:16:53 +0100

    ajout des composants

---
+++ src/app/top-salesperson/top-salesperson.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-salesperson',
+  templateUrl: './top-salesperson.component.html',
+  styleUrls: ['./top-salesperson.component.css']
+})
+export class TopSalespersonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

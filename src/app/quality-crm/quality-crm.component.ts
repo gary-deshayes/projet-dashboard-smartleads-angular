a4b8abcd77f7edc8b4acf43a4a7d3dd62840d0ed
Maxime DUROYON <maxime.duroyonJR@gmail.com>
2019-03-12 14:57:31 +0100

    ajout des composants

---
+++ src/app/quality-crm/quality-crm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quality-crm',
+  templateUrl: './quality-crm.component.html',
+  styleUrls: ['./quality-crm.component.scss']
+})
+export class QualityCrmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

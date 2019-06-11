c8ffa6f9a0f889f56f4200ef324dbacac06d5a68
Baptiste <baptiste.rossignol@hotmail.fr>
2019-06-11 14:58:46 +0200

    UPD: Component

--- src/app/performance-map/performance-map.component.ts
+++ src/app/performance-map/performance-map.component.ts
@@ -10,19 +10,7 @@ export class PerformanceMapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  //   $(document).ready(function () {
-  //     $('#francemap').vectorMap({
-  //         map: 'france_fr',
-  //         hoverOpacity: 0.5,
-  //         hoverColor: false,
-  //         backgroundColor: "#ffffff",
-  //         colors: couleurs,
-  //         borderColor: "#000000",
-  //         selectedColor: "#EC0000",
-  //         enableZoom: false,
-  //         showTooltip: true
-  //     });
-  // });
+    
   }
 
 }

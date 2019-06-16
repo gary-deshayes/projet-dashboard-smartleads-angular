f4975d0b313f2173fdcb22ea6463d40965c82944
Baptiste Rossignol <baptiste.rossignol@hotmail.fr>
2019-06-16 21:53:10 +0200

    Revert "Voir description"
    This reverts commit cc681f32cdeb306eb1ac2d8af68bce213d4f6b92.

--- src/app/quality-crm/quality-crm.component.ts
+++ src/app/quality-crm/quality-crm.component.ts
@@ -1,5 +1,4 @@
-import { ApiConnectionService } from './../api-connection.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quality-crm',
@@ -8,42 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class QualityCrmComponent implements OnInit {
 
-  @Input() apiConnection: ApiConnectionService;
-
-  private loading = false;
-  private data: any;
-  private url = "api/contacts/indiceCRM"
-
   constructor() { }
 
   ngOnInit() {
-    if(this.apiConnection.getIfConnected()){
-      this.getDataCrm();
-    }
-  }
-
-  getDataCrm(){
-    this.loading = true;
-    this.apiConnection.getData(this.url).subscribe((dataApi) => {
-      this.data = dataApi;
-      this.loading = false;
-      console.log(this.data);
-
-
-    },
-      (error) => {
-        if(error.status == 401){
-          this.apiConnection.deconnected();
-          localStorage.setItem('token', undefined);
-        }
-
-      this.loading = false;
-
-      },
-      () => {
-        this.loading = false;
-    });
-
   }
 
 }
